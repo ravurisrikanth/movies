d6d708726d9f0d3fc1220b5e50a3f684238c2415
ravurisrikanth <srikanthravuri8889@gmail.com>
2024-06-08 20:30:10 -0500

    Initial commit

---
+++ movies/src/main/java/com/srikanth/movies/MovieController.java
@@ -0,0 +1,33 @@
+package com.srikanth.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    private ResponseEntity<List<Movie>> showMovies(){
+       return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    private ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.getSingleMovie(imdbId), HttpStatus.OK);
+    }
+
+    @GetMapping("/name/{title}")
+     private ResponseEntity<Optional<Movie>> getMovieByTitle(@PathVariable String title){
+        return new ResponseEntity<Optional<Movie>>(movieService.getMovieByTitle(title), HttpStatus.OK);
+    }
+
+}
---
+++ movies/src/main/java/com/srikanth/movies/MovieService.java
@@ -0,0 +1,28 @@
+package com.srikanth.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getSingleMovie(String imdbId){
+        return movieRepository.getMovieByImdbId(imdbId);
+    }
+
+    public Optional<Movie> getMovieByTitle( String title){
+        return movieRepository.getMovieByTitle(title);
+    }
+
+
+}
