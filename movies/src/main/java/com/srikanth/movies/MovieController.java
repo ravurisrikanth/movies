cfd75b5baed605ddd468fb310bc714eea0da9eb4
ravurisrikanth <srikanthravuri8889@gmail.com>
2024-06-09 12:01:21 -0500

    Just added coment

--- movies/src/main/java/com/srikanth/movies/MovieController.java
+++ movies/src/main/java/com/srikanth/movies/MovieController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+//Rest controler
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
